a96557dcf7846d65830d982df1582b8680a4211b
Tejeshwar Reddy <mbtejeshwarreddy@gmail.com>
2021-06-15 23:16:25 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FileUpload from "./components/file-upload/file-upload.component";
+import Navbar from './components/navigation/navbar/navbar';
+import 'antd/dist/antd.css'; 
+import { Button, message } from "antd";
 
 function App() {
+  const [newUserInfo, setNewUserInfo] = useState({
+    profileImages: []
+  });
+
+  const updateUploadedFiles = (files) =>
+    setNewUserInfo({ ...newUserInfo, profileImages: files });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //logic to create new user...
+  };
+
+  const showSuccessMessage = ()=>{
+    message.success("Files uploaded succesfully")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div style={{display:'flex', 'margin-top':'5%'}}>
+      <div style={{width:'50%', margin: '10px'}}>
+
+      <FileUpload
+          accept=".jpg,.png,.jpeg"
+          label="Upload First Image"
+          multiple
+          updateFilesCb={updateUploadedFiles}
+        />
+      </div>
+
+
+    <div style={{width:'50%', margin: '10px'}}>
+    <FileUpload
+          accept=".jpg,.png,.jpeg"
+          label="Upload Second Image"
+          multiple
+          updateFilesCb={updateUploadedFiles}
+        />
+    </div>
+
+      </div>
+      <form onSubmit={handleSubmit}>
+      <div style = {{textAlign:"center"}}>
+
+      <Button  type="primary" onClick={showSuccessMessage}>
+              Upload and compare images
+            </Button>
+        
+      </div>
+
+      </form>
     </div>
   );
 }
---
+++ src/components/navigation/navbar/navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import styles from "./styles.module.css";
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+  }
+
+  handleLogout = () => {
+
+  };
+
+  render() {
+    debugger;
+    return (
+      <header className={styles["header"]}>
+        <div className={styles["left-container"]}>
+          <div className={styles["text-big"]}>Fake Image detector</div>
+          {this.props.heading && (
+            <h1 className={`${styles["text-big"]} ${styles["heading"]}`}>
+              {this.props.heading}
+            </h1>
+          )}
+        </div>
+
+        <div className={styles["right-container"]}>
+         
+        </div>
+      </header>
+    );
+  }
+}
+
+// export default withRouter(Navigation);
+export default Navigation;
